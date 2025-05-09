0361dcbfb18476514e100f4e1215efbc11ad5756
Adrien 42 <adr.bidaux@gmail.com>
2025-05-09 17:26:44 +0200

    push

--- src/philosopher.h
+++ src/philosopher.h
@@ -6,7 +6,7 @@
 /*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 14:54:24 by abidaux           #+#    #+#             */
-/*   Updated: 2025/04/30 12:49:03 by abidaux          ###   ########.fr       */
+/*   Updated: 2025/04/30 12:56:40 by abidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,9 +42,11 @@ typedef struct s_rules
 	pthread_mutex_t	*forks;
 } t_rules;
 
-bool	input_is_ok(int ac, char **av);
-t_rules	*init_rules(int ac, char **av);
-bool	is_number(char *number);
-int		ft_atoi(char *nbr);
-void	free_philo(t_rules *philo, char *msg_error);
-void	better_usleep(long long ms);
+bool			input_is_ok(int ac, char **av);
+t_rules			*init_rules(int ac, char **av);
+bool			is_number(char *number);
+int				ft_atoi(char *nbr);
+void			free_philo(t_rules *philo, char *msg_error);
+void			better_usleep(long long ms);
+t_philo			*init_philos(t_rules *rules, pthread_mutex_t *forks);
+pthread_mutex_t	*init_forks(t_rules *rules);
